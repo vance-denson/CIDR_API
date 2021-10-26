1637706fd7c5586cd3944d29c0ced2c1ea4fd42a
vance-denson <vadeson17@protonmail.com>
2021-10-26 12:04:25 -0400

    uncommenting DELETE tests

--- dist/test/delete/delete.test.js
+++ dist/test/delete/delete.test.js
@@ -1,106 +1,106 @@
 "use strict";
-// import { startServer } from '../../app';
-// import ChaiHttp from 'chai-http';
-// let chai = require('chai');
-// let chaiHttp = require('chai-http');
-// let should = chai.should();
-// chai.use(chaiHttp);
-// startServer;
-// const endpoint: string = process.env.TEST_PATH || 'localhost:3000/api/cidr';
-// describe('-- DELETE TESTS --', () => {
-//   describe('Route /', () => {
-//     describe('Setup', () => {
-//       it('test db created', (done) => {
-//         chai
-//           .request(endpoint)
-//           .post('/')
-//           .send({ address: '10.1.1.1/30' })
-//           .end((err: Error, res: ChaiHttp.Response) => {
-//             res.should.have.status(201);
-//             done();
-//           });
-//       });
-//     });
-//     describe('Test', () => {
-//       it('Delete first IP - status 200, success TRUE', (done) => {
-//         chai
-//           .request(endpoint)
-//           .delete('/')
-//           .send({ address: '10.1.1.1' })
-//           .end((err: Error, res: ChaiHttp.Response) => {
-//             res.should.have.status(200);
-//             res.body.should.have.property('success', true);
-//             done();
-//           });
-//       });
-//       it('Delete first IP - status 404, success FALSE', (done) => {
-//         chai
-//           .request(endpoint)
-//           .delete('/')
-//           .send({ address: '10.1.1.1' })
-//           .end((err: Error, res: ChaiHttp.Response) => {
-//             res.should.have.status(404);
-//             res.body.should.have.property('success', false);
-//             done();
-//           });
-//       });
-//       it('Delete Second IP - status 200, success TRUE', (done) => {
-//         chai
-//           .request(endpoint)
-//           .delete('/')
-//           .send({ address: '10.1.1.2' })
-//           .end((err: Error, res: ChaiHttp.Response) => {
-//             res.should.have.status(200);
-//             res.body.should.have.property('success', true);
-//             done();
-//           });
-//       });
-//     });
-//   });
-//   describe('Route /:IP', () => {
-//     describe('Setup', () => {
-//       it('test db created', (done) => {
-//         chai
-//           .request(endpoint)
-//           .post('/')
-//           .send({ address: '10.1.1.1/30' })
-//           .end((err: Error, res: ChaiHttp.Response) => {
-//             res.should.have.status(201);
-//             done();
-//           });
-//       });
-//     });
-//     describe('Test', () => {
-//       it('Delete first IP - status 200, success TRUE', (done) => {
-//         chai
-//           .request(endpoint)
-//           .delete('/10.1.1.1')
-//           .end((err: Error, res: ChaiHttp.Response) => {
-//             res.should.have.status(200);
-//             res.body.should.have.property('success', true);
-//             done();
-//           });
-//       });
-//       it('Delete first IP - status 404, success FALSE', (done) => {
-//         chai
-//           .request(endpoint)
-//           .delete('/10.1.1.1')
-//           .end((err: Error, res: ChaiHttp.Response) => {
-//             res.should.have.status(404);
-//             res.body.should.have.property('success', false);
-//             done();
-//           });
-//       });
-//       it('Delete Second IP - status 200, success TRUE', (done) => {
-//         chai
-//           .request(endpoint)
-//           .delete('/10.1.1.2')
-//           .end((err: Error, res: ChaiHttp.Response) => {
-//             res.should.have.status(200);
-//             res.body.should.have.property('success', true);
-//             done();
-//           });
-//       });
-//     });
-//   });
-// });
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = require("../../app");
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let should = chai.should();
+chai.use(chaiHttp);
+app_1.startServer;
+const endpoint = process.env.TEST_PATH || 'localhost:3000/api/cidr';
+describe('-- DELETE TESTS --', () => {
+    describe('Route /', () => {
+        describe('Setup', () => {
+            it('test db created', (done) => {
+                chai
+                    .request(endpoint)
+                    .post('/')
+                    .send({ address: '10.1.1.1/30' })
+                    .end((err, res) => {
+                    res.should.have.status(201);
+                    done();
+                });
+            });
+        });
+        describe('Test', () => {
+            it('Delete first IP - status 200, success TRUE', (done) => {
+                chai
+                    .request(endpoint)
+                    .delete('/')
+                    .send({ address: '10.1.1.1' })
+                    .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('success', true);
+                    done();
+                });
+            });
+            it('Delete first IP - status 404, success FALSE', (done) => {
+                chai
+                    .request(endpoint)
+                    .delete('/')
+                    .send({ address: '10.1.1.1' })
+                    .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.have.property('success', false);
+                    done();
+                });
+            });
+            it('Delete Second IP - status 200, success TRUE', (done) => {
+                chai
+                    .request(endpoint)
+                    .delete('/')
+                    .send({ address: '10.1.1.2' })
+                    .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('success', true);
+                    done();
+                });
+            });
+        });
+    });
+    describe('Route /:IP', () => {
+        describe('Setup', () => {
+            it('test db created', (done) => {
+                chai
+                    .request(endpoint)
+                    .post('/')
+                    .send({ address: '10.1.1.1/30' })
+                    .end((err, res) => {
+                    res.should.have.status(201);
+                    done();
+                });
+            });
+        });
+        describe('Test', () => {
+            it('Delete first IP - status 200, success TRUE', (done) => {
+                chai
+                    .request(endpoint)
+                    .delete('/10.1.1.1')
+                    .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('success', true);
+                    done();
+                });
+            });
+            it('Delete first IP - status 404, success FALSE', (done) => {
+                chai
+                    .request(endpoint)
+                    .delete('/10.1.1.1')
+                    .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.have.property('success', false);
+                    done();
+                });
+            });
+            it('Delete Second IP - status 200, success TRUE', (done) => {
+                chai
+                    .request(endpoint)
+                    .delete('/10.1.1.2')
+                    .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('success', true);
+                    done();
+                });
+            });
+        });
+    });
+});
--- src/test/delete/delete.test.ts
+++ src/test/delete/delete.test.ts
@@ -1,108 +1,108 @@
-// import { startServer } from '../../app';
-// import ChaiHttp from 'chai-http';
-// let chai = require('chai');
-// let chaiHttp = require('chai-http');
+import { startServer } from '../../app';
+import ChaiHttp from 'chai-http';
+let chai = require('chai');
+let chaiHttp = require('chai-http');
 
-// let should = chai.should();
-// chai.use(chaiHttp);
+let should = chai.should();
+chai.use(chaiHttp);
 
-// startServer;
-// const endpoint: string = process.env.TEST_PATH || 'localhost:3000/api/cidr';
+startServer;
+const endpoint: string = process.env.TEST_PATH || 'localhost:3000/api/cidr';
 
-// describe('-- DELETE TESTS --', () => {
-//   describe('Route /', () => {
-//     describe('Setup', () => {
-//       it('test db created', (done) => {
-//         chai
-//           .request(endpoint)
-//           .post('/')
-//           .send({ address: '10.1.1.1/30' })
-//           .end((err: Error, res: ChaiHttp.Response) => {
-//             res.should.have.status(201);
-//             done();
-//           });
-//       });
-//     });
-//     describe('Test', () => {
-//       it('Delete first IP - status 200, success TRUE', (done) => {
-//         chai
-//           .request(endpoint)
-//           .delete('/')
-//           .send({ address: '10.1.1.1' })
-//           .end((err: Error, res: ChaiHttp.Response) => {
-//             res.should.have.status(200);
-//             res.body.should.have.property('success', true);
-//             done();
-//           });
-//       });
-//       it('Delete first IP - status 404, success FALSE', (done) => {
-//         chai
-//           .request(endpoint)
-//           .delete('/')
-//           .send({ address: '10.1.1.1' })
-//           .end((err: Error, res: ChaiHttp.Response) => {
-//             res.should.have.status(404);
-//             res.body.should.have.property('success', false);
-//             done();
-//           });
-//       });
-//       it('Delete Second IP - status 200, success TRUE', (done) => {
-//         chai
-//           .request(endpoint)
-//           .delete('/')
-//           .send({ address: '10.1.1.2' })
-//           .end((err: Error, res: ChaiHttp.Response) => {
-//             res.should.have.status(200);
-//             res.body.should.have.property('success', true);
-//             done();
-//           });
-//       });
-//     });
-//   });
-//   describe('Route /:IP', () => {
-//     describe('Setup', () => {
-//       it('test db created', (done) => {
-//         chai
-//           .request(endpoint)
-//           .post('/')
-//           .send({ address: '10.1.1.1/30' })
-//           .end((err: Error, res: ChaiHttp.Response) => {
-//             res.should.have.status(201);
-//             done();
-//           });
-//       });
-//     });
-//     describe('Test', () => {
-//       it('Delete first IP - status 200, success TRUE', (done) => {
-//         chai
-//           .request(endpoint)
-//           .delete('/10.1.1.1')
-//           .end((err: Error, res: ChaiHttp.Response) => {
-//             res.should.have.status(200);
-//             res.body.should.have.property('success', true);
-//             done();
-//           });
-//       });
-//       it('Delete first IP - status 404, success FALSE', (done) => {
-//         chai
-//           .request(endpoint)
-//           .delete('/10.1.1.1')
-//           .end((err: Error, res: ChaiHttp.Response) => {
-//             res.should.have.status(404);
-//             res.body.should.have.property('success', false);
-//             done();
-//           });
-//       });
-//       it('Delete Second IP - status 200, success TRUE', (done) => {
-//         chai
-//           .request(endpoint)
-//           .delete('/10.1.1.2')
-//           .end((err: Error, res: ChaiHttp.Response) => {
-//             res.should.have.status(200);
-//             res.body.should.have.property('success', true);
-//             done();
-//           });
-//       });
-//     });
-//   });
-// });
+describe('-- DELETE TESTS --', () => {
+  describe('Route /', () => {
+    describe('Setup', () => {
+      it('test db created', (done) => {
+        chai
+          .request(endpoint)
+          .post('/')
+          .send({ address: '10.1.1.1/30' })
+          .end((err: Error, res: ChaiHttp.Response) => {
+            res.should.have.status(201);
+            done();
+          });
+      });
+    });
+    describe('Test', () => {
+      it('Delete first IP - status 200, success TRUE', (done) => {
+        chai
+          .request(endpoint)
+          .delete('/')
+          .send({ address: '10.1.1.1' })
+          .end((err: Error, res: ChaiHttp.Response) => {
+            res.should.have.status(200);
+            res.body.should.have.property('success', true);
+            done();
+          });
+      });
+      it('Delete first IP - status 404, success FALSE', (done) => {
+        chai
+          .request(endpoint)
+          .delete('/')
+          .send({ address: '10.1.1.1' })
+          .end((err: Error, res: ChaiHttp.Response) => {
+            res.should.have.status(404);
+            res.body.should.have.property('success', false);
+            done();
+          });
+      });
+      it('Delete Second IP - status 200, success TRUE', (done) => {
+        chai
+          .request(endpoint)
+          .delete('/')
+          .send({ address: '10.1.1.2' })
+          .end((err: Error, res: ChaiHttp.Response) => {
+            res.should.have.status(200);
+            res.body.should.have.property('success', true);
+            done();
+          });
+      });
+    });
+  });
+  describe('Route /:IP', () => {
+    describe('Setup', () => {
+      it('test db created', (done) => {
+        chai
+          .request(endpoint)
+          .post('/')
+          .send({ address: '10.1.1.1/30' })
+          .end((err: Error, res: ChaiHttp.Response) => {
+            res.should.have.status(201);
+            done();
+          });
+      });
+    });
+    describe('Test', () => {
+      it('Delete first IP - status 200, success TRUE', (done) => {
+        chai
+          .request(endpoint)
+          .delete('/10.1.1.1')
+          .end((err: Error, res: ChaiHttp.Response) => {
+            res.should.have.status(200);
+            res.body.should.have.property('success', true);
+            done();
+          });
+      });
+      it('Delete first IP - status 404, success FALSE', (done) => {
+        chai
+          .request(endpoint)
+          .delete('/10.1.1.1')
+          .end((err: Error, res: ChaiHttp.Response) => {
+            res.should.have.status(404);
+            res.body.should.have.property('success', false);
+            done();
+          });
+      });
+      it('Delete Second IP - status 200, success TRUE', (done) => {
+        chai
+          .request(endpoint)
+          .delete('/10.1.1.2')
+          .end((err: Error, res: ChaiHttp.Response) => {
+            res.should.have.status(200);
+            res.body.should.have.property('success', true);
+            done();
+          });
+      });
+    });
+  });
+});
